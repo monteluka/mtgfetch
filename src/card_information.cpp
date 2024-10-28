0928e709af7b428dec4102715f1121c23865d516
Monte <dmonterrosa@pm.me>
2024-10-28 12:46:52 -0700

    loadInfo can now handle maps inside map nodes

--- src/card_information.cpp
+++ src/card_information.cpp
@@ -5,8 +5,8 @@ bool loadInfo(std::vector<std::string>& information, const ryml::Tree& card, con
 {
     static int depth {};
     ++depth;
-
     c4::csubstr key {};
+    std::string info {};
 
     if (configNode.has_key())
     {
@@ -18,15 +18,24 @@ bool loadInfo(std::vector<std::string>& information, const ryml::Tree& card, con
     }
     else
     {
+        std::cout << configNode << std::endl;
+        std::cout << configNode.num_children() << std::endl;
+        std::cout << configNode.is_seq() << std::endl;
+        std::cout << configNode.is_map() << std::endl;
         std::cout << "node has no value or key" << std::endl;
+        --depth;
+        for (const c4::yml::ConstNodeRef child : configNode)
+        {
+            loadInfo(information, card, child);
+        }
         return false;
     }
 
-    std::cout << "Number of children: " << card[key].num_children() << std::endl;
+    std::cout << card << std::endl;
+    std::cout << card.num_children(card.root_id()) << std::endl;
 
     const c4::yml::ConstNodeRef el = card[key];
 
-    std::string info {};
     if (depth < 2)
     {
         info += std::string(key.str, key.len) + ": ";
@@ -117,27 +126,41 @@ void appendSequence(std::vector<std::string>& information,
                 info += std::string(2, ' ') + parentNodeKey;
                 information.push_back(info);
 
+                std::cout << configNode << std::endl;
+
                 for (const auto& configNodeChild : configNode.children())
                 {
-                    if (element[configNodeChild.val()].is_seq())
+                    c4::csubstr key {};
+                    std::cout << configNodeChild << std::endl;
+                    std::cout << configNodeChild.has_val() << " " << configNodeChild.has_key() << std::endl;
+                    if (configNodeChild.has_val())
                     {
-                        if (element[configNodeChild.val()].num_children() > 0)
+                        key = configNodeChild.val();
+                    }
+                    else
+                    {
+                        key = configNodeChild[0].key();
+                    }
+                    std::cout << key << std::endl;
+                    if (element[key].is_seq())
+                    {
+                        if (element[key].num_children() > 0)
                         {
-                            c4::yml::NodeRef newSeqNode = parentNode[element[configNodeChild.val()].key()];
+                            c4::yml::NodeRef newSeqNode = parentNode[element[key].key()];
                             newSeqNode |= c4::yml::SEQ;
-                            for (const auto& seqChildNode : element[configNodeChild.val()].children())
+                            for (const auto& seqChildNode : element[key].children())
                             {
                                 if (!seqChildNode.val_is_null()) newSeqNode.append_child() = seqChildNode.val();
                             }
                         }
                     }
-                    else if (element[configNodeChild.val()].is_map())
+                    else if (element[key].is_map())
                     {
-                        if (element[configNodeChild.val()].num_children() > 0)
+                        if (element[key].num_children() > 0)
                         {
-                            c4::yml::NodeRef mapNode = parentNode[element[configNodeChild.val()].key()];
+                            c4::yml::NodeRef mapNode = parentNode[element[key].key()];
                             mapNode |= c4::yml::MAP;
-                            for (const auto& mapChildNode : element[configNodeChild.val()].children())
+                            for (const auto& mapChildNode : element[key].children())
                             {
                                 mapNode[mapChildNode.key()] = mapChildNode.val();
                             }
@@ -145,7 +168,7 @@ void appendSequence(std::vector<std::string>& information,
                     }
                     else
                     {
-                        parentNode[element[configNodeChild.val()].key()] << element[configNodeChild.val()].val();
+                        parentNode[element[key].key()] << element[key].val();
                     }
                 }
                 for (const auto& key : configNode)
